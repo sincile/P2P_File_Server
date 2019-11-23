66bb07b38743794ed20370a162eff1c01565c16e
Dylan15145 <dylan15145@gmail.com>
2019-11-23 10:22:14 -0500

    Update server2.py

--- server2.py
+++ server2.py
@@ -2,36 +2,74 @@ import threading
 import socket
 import sys
 import Library
+import os
 
 host = '127.0.0.1'
 # ports = [a for a in range(1024,65536)]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind((host, 8393))
+server.bind((host, 8118))
 server.listen(5)
 
+
+def PWD():
+    try:
+        currentDir = os.getcwd()
+    except:
+        error = 'getcwd error'
+        return error
+    return currentDir
+        
+    
+def DIR():
+    try:
+        directoryListing = os.listdir()
+    except:
+        error = 'listdir error'
+        return error
+        
+    return directoryListing
+    
+def CD(connection):
+    newDirectory = Library.read(connection)
+    
+    try:
+        os.chdir(newDirectory)
+    except:
+        print ('chdir error')
+        
+    return
+
 def main():
+
+    connection, addr = server.accept()
+    data = Library.read(connection)
+    print (data)
+    hello = "Hello from the server"
+    Library.write(hello, connection)
+    
     while True:
-        connection, addr = server.accept()
-        clientMessage  = b''
-        
-        #while True:
+    
+        clientMessage  = ''
         data = Library.read(connection)
-			#if (not data):
-                #clientMessage += str(data.decode('utf-8'))
-				#break
-        print (data)
         
-        hello = "Hello from the server"
-        hello = hello.encode('utf-8')
+        if data == 'PWD':
+            currentDir = PWD()
+            Library.write(currentDir, connection)
+        elif data == 'DIR':
+            directoryListing = DIR()
+            Library.write(directoryListing, connection)
+        elif data == 'CD':
+            CD(connection)
+        elif (data == 'BYE'):
+            print (data)
+            break
         
-        connection.sendall(hello)
 		
         
-        connection.close()
-        print('Client disconnected\n')
-        break
+    connection.close()
+    print('Client disconnected\n')
 
 
 if __name__ == '__main__':
