3a8a8c4db29dec032f213ad4b0527fa028a6d7af
GitHubFan14 <48738612+GitHubFan14@users.noreply.github.com>
2019-11-25 19:06:32 -0500

    Add files via upload
    Includes comments and updated blocks for header and functions.

--- Library.py
+++ Library.py
@@ -1,56 +1,49 @@
-#***************************************************************
-#* 
+#*******************************************************************************
 #* Author: Brandon Kresge
 #* Creation Date: November 19, 2019
 #* Due Date: November 26th, 2019
 #* Python Version: 3.7.3
 #* Course: CSC328-010
 #* Filename: LibraryTest.py
-#* Purpose: Make a library that the client and server can call functions from.
-#* To run and compile: ?
-#**************************************************************
+#* Purpose: Makes a shared library that the client and server can call functions from.
+#* To run and compile: N/A; used when inputting python3 server.py and python3 client.py
+#*******************************************************************************
+
+# this file is shared, as Python libraries are shared by default
 
 import os
 import socket
-#import socketUtils
-terminatingstring = "*?*"
+terminatingstring = "*?*" # string of characters used to know if end of file, since the string should not be in a file
 
-#**************************************************************
-#Function Name: read
-#Description: Reads message to socket
-#Parameters: None
-#Return value: None
-#**************************************************************
-
-#def read():    
-
-#make sure in read byte mode
-
-#**************************************************************
-#Function Name: write
-#Description: Writes message to socket
-#Parameters: None
-#Return value: None
-#**************************************************************
+#**************************************************************************
+#* Function Name: write
+#* Description: Writes message to socket
+#* Parameters: message - what is being sent to the socket, sock - a socket
+#* Return value: None
+#*************************************************************************
 
 def write(message, sock):    
-    message = str(message) + terminatingstring
+    message = str(message) + terminatingstring # attach terminatingstring to message
     
-    message = message.encode('utf-8')
+    message = message.encode('utf-8') # encode message
     try:
-        sock.sendall(message)
+        sock.sendall(message) # send message
     except:
         print ("Sendall error")
     return
-#write byte mode
 
-#read function
+#******************************************************************************************
+#Function Name: read
+#Description: Reads message from socket
+#Parameters: sock - a socket
+#Return value: message[:-3] - the message being read, but the last three characters removed because the message has the terminating character attached
+#******************************************************************************************
 
 def read(sock):
     message = ''
     while True:
         try:
-            message = message + sock.recv(256).decode('utf-8')
+            message = message + sock.recv(256).decode('utf-8') # receive and decode message
         except:
             print ("recv error")
             break
@@ -58,16 +51,16 @@ def read(sock):
             break
     return message[:-3]
 
-#**************************************************************
+#******************************************************************************************
 #Function Name: validate
 #Description: Validates input. If input is not from the list of commands, returns an error.
 #Parameters: input - user input
-#Return value: None
-#**************************************************************
+#Return value: valid - input from user that is from the list of commands
+#******************************************************************************************
 
 def validate(input):
     input = input.upper()
-    input = input.split(' ', 1)[0]
+    input = input.split(' ', 1)[0] # split command from file and directory or anything else aftter the command name
     print (input)
     if input == "PWD":
         valid = True
@@ -91,30 +84,4 @@ def validate(input):
         valid = True
     else: 
         valid = False
-    return valid    
-
-#def PWD(): # also handles LPWD
-#    return os.getcwd()
-
-# DOWNLOAD
-
-
-#def DIR (localdirectory):
-#    return os.listdir(localdirectory)
-
-# CD
-
-
-# LDIR
-
-
-# LCD
-
-
-#**************************************************************
-#Function Name: errorchecking
-#Description: Checks for errors
-#Parameters: None
-#Return value: None
-#**************************************************************
-#def errorchecking()
+    return valid
