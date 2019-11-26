f00ff89742c9f963ed43063d8f97b60b8c180841
christian <cclar612@live.kutztown.edu>
2019-11-25 23:20:53 -0500

    Added download, cd and user specific paths

--- server_iteration/multiport_server.py
+++ server_iteration/multiport_server.py
@@ -7,44 +7,48 @@ import Library
 
 host = '127.0.0.1'
 terminating_char = "*?*"
+# default_path = "/Users/Christian"
+default_path = "/export/home/public"
 
 def hello(connect, message="Hello from the server"):
     Library.write(message, connect)
 
-def pwd(connect):
+def pwd(connect,path=default_path):
     try:
-        path = os.getcwd()
         Library.write(path, connect)
         print("Sent:", path)
     except Exception:
         Library.write("Error getting current path", connect)
 
-def dir(connect,path=''):
+def dir(connect,path=default_path):
     try:
-        if(path):
-            path_content = os.listdir(path)
-        else:
-            path_content = os.listdir()
+        path_content = os.listdir(str(path))
         Library.write(path_content, connect)
         print("Sent:", path_content)
     except Exception:
         Library.write("Error getting current path", connect)
 
-def cd(connect,path=''):
+def cd(connect,path=default_path):
     try:
-        if(path):
-            path_content = os.chdir(path)
-        else:
-            path_content = os.chdir('/')
-        Library.write(path_content, connect)
-        print("Sent:", path_content)
-        return path_content
-    except Exception:
-        Library.write("Error getting current path", connect)
+        path_content = os.chdir(str(path))
+        Library.write('0', connect)
+        print("Sent: ", '0')
+        #reset server location back to origin
+        os.chdir(default_path)
+        return 0
+    except (Exception, FileNotFoundError, PermissionError) as e:
+        Library.write('-1', connect)
+        print("Sent: ", '-1')
+        return -1
 
-def download(connect,path='/',file='*'):
-    Library.write("This don't work yet chief", connect)
-    print("Sent: " + "nothing lmao")
+def check_file(path,file):
+    return os.path.isfile(path+'/'+file)
+
+def download(connect,path=default_path,file='*'):
+    with open(path+'/'+file, 'r') as file_content:
+        content = file_content.read()
+    Library.write(content, connect)
+    print("Sent: ", content)
 
 def create_socket(portNumber):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,8 +57,8 @@ def create_socket(portNumber):
     try:
         server.bind((host, portNumber))
     except socket.error:
-        print("Error when binding")
-        sys.exit()
+        print("Error when binding on port" + portNumber)
+        return
     print("Socket bound")
     server.listen(10)
     print("Socket is listening on: ", portNumber)
@@ -63,28 +67,49 @@ def create_socket(portNumber):
 
 def client_connection(connect):
     hello(connect)
-    currentDir = '/'
+    currentDir = default_path
     while True:
         try:
             data = Library.read(connect)
             server_reply = "Got: " + data
+            print(server_reply)
+
             if data == 'PWD':
-                pwd(connect)
+                pwd(connect,currentDir)
             if data == 'DIR':
-                dir(connect)
+                dir(connect,currentDir)
             if data == 'CD':
-                data = Library.read(connect)
-                currentDir = cd(connect,data)
+                path = Library.read(connect)
+                print("Got directory: " + path)
+                if (cd(connect, path) != -1):
+                    currentDir = path
+                else:
+                    pass
             if data == 'DOWNLOAD':
-                download(connect)
+                file = Library.read(connect)
+                print("File: " + file)
+                if (check_file(currentDir,file)):
+                    Library.write('READY',connect)
+                    print('File exists')
+                    clientStatus = Library.read(connect)
+                    if (clientStatus == "READY"):
+                        print('Client confirmed...starting download')
+                        download(connect,currentDir,file);
+                    else:
+                        print('Client Denied')
+                        pass
+                else:
+                    print('File Not Found')
+                    Library.write('File Not Found',connect)
+                    pass
             if data == 'BYE':
+                print("See you later boo")
                 connect.close()
                 break
             if not data:
+                print("It's empty, closing the port")
                 connect.close()
                 break
-            print(server_reply)
-            # connect.send(("Server Recieved: " + server_reply + terminating_char).encode('utf-8'))
         except socket.error:
             print("Client disconnected")
             connect.close()
@@ -92,14 +117,19 @@ def client_connection(connect):
             print("PIPE error")
             connect.close()
 
-
 def main():
     connection_list = []
+    error_list = []
     for port in range(8000,8101):
           try:
               connection_list.append(create_socket(port))
           except Exception:
+              print("Cannot bind port: ", port)
+              error_list.append(port)
               pass
+
+    if error_list:
+        print("NOTE that these ports are not bound due to an unknown error: ", error_list)
 
     while True:
         #Monitor all conenction ports for activity
@@ -109,7 +139,7 @@ def main():
         for sock in s_read:
             connect, addr = sock.accept()
             try:
-                #auto handle threads using the client connection
+                #auto handle threads using the client connection func
                 start_new_thread(client_connection, (connect,))
             except Exception:
                 print("Can't create client thread")
